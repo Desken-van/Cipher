f781c2a5400f7dcc1b253d4cab3cac404a4bb762
Desken-van <79146520+Desken-van@users.noreply.github.com>
2021-05-12 13:15:02 +0300

    Add files via upload

--- Controllers/ServerController.cs
+++ Controllers/ServerController.cs
@@ -13,6 +13,8 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Text;
 using Crypt;
+using System.Security.Cryptography;
+using Ionic.Zip;
 
 namespace Server.Controllers
 {
@@ -22,25 +24,52 @@ namespace Server.Controllers
         [HttpPost("/GetFile")]
         public IActionResult GetFile(IFormFile file)
         {
-            string path = @"S:\Work\Serv\Server\Upload";
+            string path = @"D:\Labs\Serv\Server\Upload";
             string filePath = Path.Combine(path, file.FileName);
             using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
             {
                 file.CopyToAsync(fileStream);               
             }
             string type = file.Headers.Values.Last();
-            string root = @"S:\Work\Serv\Server\Upload\Uncrypt" + type;
+            string root = @"D:\Labs\Serv\Server\Upload\UncryptFile" + type;
+            string sign = @"D:\Labs\Serv\Server\Upload\SignedFile.cig";
+            string cryptppath = @"D:\Labs\Serv\Server\Upload\CryptZip.zip";
+            string zipstr = @"D:\Labs\Serv\Server\Upload\ZIP.zip";
             Crypted.DecryptFile(filePath, root);
             FileInfo Delete = new FileInfo(filePath);
             Delete.Delete();
             FileStream stream = new FileStream(root, FileMode.Open, FileAccess.Read);
             using (stream)
             {
-                byte[] array = new byte[stream.Length];
+                byte[] array = new byte[stream.Length];                
                 stream.Read(array, 0, array.Length);
-                return File(array, "text/plain", file.FileName);
+                byte[] signed = SignDoc.Sign(array);
+                SignDoc.WriteFile(sign, signed);     
             }
-        }
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AddDirectory(path+"\\");
+                zip.Save(zipstr);
+            }
+            Crypted.EncryptFile(zipstr,cryptppath);
+            byte[] result;
+            FileStream rstream = new FileStream(cryptppath, FileMode.Open, FileAccess.Read);
+            using (rstream)
+            {
+                byte[] array = new byte[rstream.Length];
+                rstream.Read(array, 0, array.Length);
+                Delete = new FileInfo(root);
+                Delete.Delete();
+                Delete = new FileInfo(sign);
+                Delete.Delete();
+                Delete = new FileInfo(zipstr);
+                Delete.Delete();
+                result = array;      
+            }
+            Delete = new FileInfo(cryptppath);
+            Delete.Delete();
+            return File(result, "application/zip", "CryptZip.zip");
+        }      
     }
 }
 
